638651f7997caa3ee261273ea39e3fcf351a427e
Freimel Jerez <frei19jerez@gmail.com>
2026-07-31 11:50:12 -0500

    Usar DATABASE_URL en producción

--- config/datastores.js
+++ config/datastores.js
@@ -1,7 +1,16 @@
 module.exports.datastores = {
   default: {
     adapter: 'sails-postgresql',
-    url: 'postgresql://postgres:emily19kenia@localhost:5432/fjia',
-    ssl: false
+
+    url:
+      process.env.DATABASE_URL ||
+      'postgresql://postgres:emily19kenia@localhost:5432/fjia',
+
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? {
+            rejectUnauthorized: false
+          }
+        : false
   }
 };
